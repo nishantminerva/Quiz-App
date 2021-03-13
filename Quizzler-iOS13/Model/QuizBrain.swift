e1734c7bcb0ebbe9cc79ab324742d9a5d85ba7d4
Nishant Kumar <53352011+nishantminerva@users.noreply.github.com>
2021-03-13 20:09:44 +0530

    Update QuizBrain.swift

--- Quizzler-iOS13/Model/QuizBrain.swift
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -14,18 +14,18 @@ struct QuizBrain {
     var score = 0
     
     let quiz = [
-        Question(q: "A slug's blood is green.", a: "True"),
-        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-        Question(q: "Google was originally called 'Backrub'.", a: "True"),
-        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+        Question(que: "A slug's blood is green.", ans: "True"),
+        Question(que: "Approximately one quarter of human bones are in the feet.", ans: "True"),
+        Question(que: "The total surface area of two human lungs is approximately 70 square metres.", ans: "True"),
+        Question(que: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", ans: "True"),
+        Question(que: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(que: "It is illegal to pee in the Ocean in Portugal.", ans: "True"),
+        Question(que: "You can lead a cow down stairs but not up stairs.", ans: "False"),
+        Question(que: "Google was originally called 'Backrub'.", ans: "True"),
+        Question(que: "Buzz Aldrin's mother's maiden name was 'Moon'.", ans: "True"),
+        Question(que: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", ans: "False"),
+        Question(que: "No piece of square dry paper can be folded in half more than 7 times.", ans: "False"),
+        Question(que: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", ans: "True")
     ]
     
     func getQuestionText() -> String {
